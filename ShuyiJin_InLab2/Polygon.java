4c30e91fa8ca3913d0aaacac06a966eb4fbf2bb5
Belinda Jin <belinda.s.jin@gmail.com>
2020-05-04 17:09:30 -0600

    Update Polygon.java

--- ShuyiJin_InLab2/Polygon.java
+++ ShuyiJin_InLab2/Polygon.java
@@ -26,19 +26,21 @@ class Polygon {
 	
     
         public String toString() {
-            // THIS METHOD DOESN'T WORK. AS PART OF EXERCISE-2 STUDENTS MUST FIX
-            // IT TO RETURN A STRING WITH THE INFORMATION ABOUT START AND END
+            // FIX IT TO RETURN A STRING WITH THE INFORMATION ABOUT START AND END
             // POINTS OF N LINES OF A POLYGON. AS SHOWN IN THE EXAMPLE BELOW:
             // The lines in polygon 1 are:
             //   Line 1: starts at (20, 30), and ends at (50, 100)
             //   Line 2: starts at (50, 100), and ends at (100, 30)
             //   Line 3: starts at (100, 30), and ends at (20, 30)
             //
-        	
-        String s = "The lines in polygon " + objID + " are: \n" + polygon.toString();        		
+            	
+		String s = "\nThe lines in polygon " + objID + " are: \n";   
+		while(it.hasNext()) {
+			s += "  " + it.next().toString();
+		}
 
-    	return s;	
-    }
+		return s;	     	
+	    }
 }
 
 
