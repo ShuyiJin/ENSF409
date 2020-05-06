ceb6dd61b988e5939bea7ae46de7fdf236d7d359
Belinda Jin <belinda.s.jin@gmail.com>
2020-05-06 15:42:39 -0600

    Update Inventory.java

--- ShuyiJin_postlab2/retailStore/src/Inventory.java
+++ ShuyiJin_postlab2/retailStore/src/Inventory.java
@@ -70,19 +70,24 @@ public class Inventory {
 		}
 	}
 	
-	public void createOrderList(Order order, String date) {
+	public OrderList createOrderList(Order order, String date) {
+		System.out.println("Order list of " + date + ":");
 		OrderList ol = new OrderList(order, date);
 		ol.generateID();
 		ol.printToFile(order);
+		return ol;
 	}
 	
-	public void orderTool(Tool item, int quant) {
+	public Order orderTool(Tool item, int quant) {
 		Order newOrder = new Order(quant);
 		newOrder.setDescription(item.getName());
+		Supplier sup = this.searchSupplierByID(item.getSupplierID());
+		newOrder.setSupplier(sup.getName());
+		return newOrder;
 	}
 	
-	public void checkToolQuantity() {
-		
+	public void checkToolQuantity(Tool item) {
+		if (item.getQuantity() < 40) System.out.println("Not enough storage.");
 	}
 	
 	public Tool searchToolByID(int id) {
@@ -92,23 +97,25 @@ public class Inventory {
 		double price = 0.0;
 		Scanner scan;
 		try {
-			scan = new Scanner(file).useDelimiter(";");
+			scan = new Scanner(file);
 			while(scan.hasNext()) {
 				final String lineFromFile = scan.nextLine();
 				if(lineFromFile.contains(Integer.toString(id))) {
-					name = scan.nextLine();
-					quant = Integer.parseInt(scan.nextLine());
-					price = Double.parseDouble(scan.nextLine());
-					supID = Integer.parseInt(scan.nextLine());
+					String[] temp = lineFromFile.split(";");
+					name = temp[1];
+					quant = Integer.parseInt(temp[2]);
+					price = Double.parseDouble(temp[3]);
+					supID = Integer.parseInt(temp[4]);
 					break;
 				}
 			}	
 		}
 		catch(IOException ex) {
+			ex.printStackTrace();
 			System.out.println(" cannot write to file " + file.toString());
 		}
 		
-		Tool tool = new Tool(id, name, quant,price,supID);
+		Tool tool = new Tool(id, name, quant, price, supID);
 		return tool;
 	}
 	
@@ -118,21 +125,22 @@ public class Inventory {
 		double price = 0.0;
 		Scanner scan;
 		try {
-			scan = new Scanner(file).useDelimiter(";");
+			scan = new Scanner(file);
 			String lineFromFile = " ";
 			while(scan.hasNext()) {
-				String temp = lineFromFile;
 				lineFromFile = scan.nextLine();
 				if(lineFromFile.contains(name)) {
-					id = Integer.parseInt(temp);
-					quant = Integer.parseInt(scan.nextLine());
-					price = Double.parseDouble(scan.nextLine());
-					supID = Integer.parseInt(scan.nextLine());
+					String[] temp = lineFromFile.split(";");
+					id = Integer.parseInt(temp[0]);
+					quant = Integer.parseInt(temp[2]);
+					price = Double.parseDouble(temp[3]);
+					supID = Integer.parseInt(temp[4]);
 					break;
 				}
 			}	
 		}
 		catch(IOException ex) {
+			ex.printStackTrace();
 			System.out.println(" cannot write to file " + file.toString());
 		}
 		
@@ -140,7 +148,60 @@ public class Inventory {
 		return tool;
 	}
 	
-	public static void main(String[] args)  {
+	public Supplier searchSupplierByID(int id) {
+		File file = new File("suppliers.txt");
+		String name = " ", address = " ", contact = " ";
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+			while(scan.hasNext()) {
+				final String lineFromFile = scan.nextLine();
+				if(lineFromFile.contains(Integer.toString(id))) {
+					String[] temp = lineFromFile.split(";");
+					name = temp[1];
+					address = temp[2];
+					contact = temp[3];
+					break;
+				}
+			}	
+		}
+		catch(IOException ex) {
+			ex.printStackTrace();
+			System.out.println(" cannot write to file " + file.toString());
+		}
 		
+		Supplier sup = new Supplier(name, address, contact, id);
+		return sup;
+	}
+	
+	public static void main(String[] args)  {
+		Inventory inv = new Inventory();
+		
+		// date 1: January 18, 2016
+		String date1 = "January 18, 2016";
+		
+		Tool tool1 = inv.searchToolByName("Nic Nacs");
+		System.out.println("Quantity of " + tool1.getName() + " is " + tool1.getQuantity());
+		Tool tool2 = inv.searchToolByID(1005);
+		System.out.println("Quantity of " + tool2.getName() + " is " + tool2.getQuantity());
+		
+		// order 250 tool1
+		Order order1 = inv.orderTool(tool1, 250);
+		OrderList ol1 = inv.createOrderList(order1, date1);
+		
+		// order 50 tool2
+		Order order2 = inv.orderTool(tool2, 50);
+		ol1.addOrder(order2);
+		
+		// date2: January 26, 2016
+		String date2 = "January 26, 2016";
+		
+		Tool tool3 = inv.searchToolByName("Wog Wits");
+		System.out.println("\nQuantity of " + tool3.getName() + " is " + tool3.getQuantity());
+		
+		
+		// order 100 tool3
+		Order order3 = inv.orderTool(tool3, 100);
+		OrderList ol2 = inv.createOrderList(order3, date2);
 	}
 }
