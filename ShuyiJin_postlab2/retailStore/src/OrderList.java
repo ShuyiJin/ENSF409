ecdf51933d4cf97f495839476940352ff491e8b6
Belinda Jin <belinda.s.jin@gmail.com>
2020-05-06 15:41:58 -0600

    Update OrderList.java

--- ShuyiJin_postlab2/retailStore/src/OrderList.java
+++ ShuyiJin_postlab2/retailStore/src/OrderList.java
@@ -7,8 +7,14 @@ import java.io.File;
 public class OrderList {
 	private int orderID;
 	private String date;
-	private Order item;
+//	private Order item;
 	private String fileName;
+	
+	public OrderList(Order order, String date) {
+//		item = order;
+		fileName = "Order - " + date + ".txt";
+		this.setDate(date);
+	}
 	
 	public void generateID() {
 		orderID = (int) Math.round(Math.random() * 89999) + 10000;
@@ -18,42 +24,44 @@ public class OrderList {
 		this.date = date;
 	}
 	
-	public OrderList(Order order, String date) {
-		item = order;
-		fileName = "Order" + date + ".txt";
-		this.setDate(date);
-	}
-	
 	public void printToFile(Order order) {
 		BufferedWriter bw = null;
 		try {
 			File file = new File(fileName);
 			bw = new BufferedWriter(new FileWriter(fileName));
-			String text = "****************************************************************";
+			String text = "****************************************************************\n";
 			bw.write(text);
-			text = "ORDER ID:			" + Integer.toString(orderID);
+			System.out.printf(text);
+			text = "ORDER ID: \t\t" + Integer.toString(orderID) + "\n";
 			bw.write(text);
-			text = "Date Ordered:		" + date;
+			System.out.printf(text);
+			text = "Date Ordered: \t\t" + date + "\n";
 			bw.write(text);
+			System.out.printf(text);
 			bw.close();
 		}
 		catch(IOException ex) {
 			ex.printStackTrace();
 		}
+		this.addOrder(order);
 	}
 	
 	public void addOrder(Order item) {
 		BufferedWriter bw = null;
 		try {
-			bw = new BufferedWriter(new FileWriter(fileName));
-			String text = " ";
+			bw = new BufferedWriter(new FileWriter(fileName, true));
+			String text = "\n";
 			bw.write(text);
-			text = "Item Description:	" + item.getDescription();
+			System.out.printf(text);
+			text = "Item Description: \t" + item.getDescription() + "\n";
+			System.out.printf(text);
 			bw.write(text);
-			text = "Amount Ordered:		" + Integer.toString(item.getQuantity());
+			text = "Amount Ordered:	\t" + Integer.toString(item.getQuantity()) + "\n";
+			System.out.printf(text);
 			bw.write(text);
-			text = "Supplier:			" + item.getSupplier();
+			text = "Supplier: \t\t" + item.getSupplier() + "\n";
 			bw.write(text);
+			System.out.printf(text);
 			bw.close();
 		}
 		catch(IOException ex) {
